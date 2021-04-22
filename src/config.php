4c3e79429e636d6a6e5c89bbfbdc183c51c2c6cb
saad <saadbinmahbub@gmail.com>
2021-04-22 12:16:22 +0600

    Add config file

---
+++ src/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'name' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Transaction parser
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of the class for parsing Transactions
+    |
+    */
+
+    'transactions_driver' => 'Acme\CommissionTask\Helpers\CSVTransactionFileParser'
+];
