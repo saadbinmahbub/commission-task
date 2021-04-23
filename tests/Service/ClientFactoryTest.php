a0e465d2ac25998d0a07636c3dfe9a4ce37da2c3
Saad Mahbub <saadbinmahbub@gmail.com>
2021-04-23 12:35:13 +0600

    Write test for client factory

--- tests/Service/ClientFactoryTest.php
+++ tests/Service/ClientFactoryTest.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 namespace Acme\CommissionTask\Tests\Service;
 
+use Acme\CommissionTask\Service\BusinessClient;
+use Acme\CommissionTask\Service\ClientFactory;
+use Acme\CommissionTask\Service\PrivateClient;
 use PHPUnit\Framework\TestCase;
 
 class ClientFactoryTest extends TestCase
 {
+    private $clientFactory;
 
     public function setUp()
     {
+        $this->clientFactory = new ClientFactory();
     }
+
+    public function testFactoryReturnsBusinessClient()
+    {
+        $businessClient = $this->clientFactory->getClient('business');
+        $this->assertInstanceOf(BusinessClient::class, $businessClient);
+    }
+
+    public function testFactoryReturnsPrivateClient()
+    {
+        $businessClient = $this->clientFactory->getClient('private');
+        $this->assertInstanceOf(PrivateClient::class, $businessClient);
+    }
+
 }
