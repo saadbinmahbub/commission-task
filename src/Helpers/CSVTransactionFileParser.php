bb97162674401fd6bbaff3870d5a1d298534993b
Saad Mahbub <saadbinmahbub@gmail.com>
2021-04-23 11:18:49 +0600

    Error message with file name

--- src/Helpers/CSVTransactionFileParser.php
+++ src/Helpers/CSVTransactionFileParser.php
@@ -12,7 +12,7 @@ class CSVTransactionFileParser implements TransactionFileParser
     public function parse(string $file): array
     {
         $transactions = [];
-        $handle = fopen($file, 'r') or die('Could not open file');
+        $handle = fopen($file, 'r') or die("Could not open file {$file}");
         for ($i = 0; $row = fgetcsv($handle); ++$i) {
             if (count($row) > 0) {
                 array_push($transactions, new Transaction($row));
