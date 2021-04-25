b99b7da604f7a7dfe1daf736f2fd9ed9fdc1938d
Saad Mahbub <saadbinmahbub@gmail.com>
2021-04-25 18:16:12 +0600

    Update ExchangeRate.php Interface to add more methods

--- src/Helpers/ExchangeRate.php
+++ src/Helpers/ExchangeRate.php
@@ -8,4 +8,10 @@ namespace Acme\CommissionTask\Helpers;
 interface ExchangeRate
 {
     public function getRates(): ExchangeRate;
+
+    public function setRates($rates);
+
+    public function convertToBaseCurrency($from, $amount): float;
+
+    public function convertFromBaseCurrency($to, $amount): float;
 }
